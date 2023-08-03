2571d50b2d5947e7e6c83c884eaf69ca0f9bb89b
Marla Laystrom <marlalaystrom@gmail.com>
2023-08-03 13:47:20 -0400

    Initial Commit

---
+++ ToDoList.WebMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ToDoList.WebMvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
